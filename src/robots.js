9305c4dbfab4c06d66182d5c83309d1bf9714e20
vijay chowdary <vijaychowdary3699@gmail.com>
2021-08-21 19:38:58 +0530

    update

--- src/robots.js
+++ src/robots.js
@@ -43,7 +43,7 @@ export const robots = [
   },
   {
     id: 8,
-    name: 'Nicholas Runolfsdottir V',
+    name: 'Nicholas V',
     username: 'Maxime_Nienow',
     email: 'Sherwood@rosamond.me'
   },
